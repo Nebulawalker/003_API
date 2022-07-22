ccf727afc73054b1419e9eba4b1a50d753ce3d10
Roman Okatev <nebulawalker@live.ru>
2022-07-22 21:37:46 +0300

    publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -8,4 +8,8 @@ load_dotenv()
 telegram_token = os.getenv("TELEGRAM_TOKEN")
 bot = telegram.Bot(token=telegram_token)
 
-bot.send_message(chat_id="@cosmos_photos", text="Это все что я пока могу")
+bot.send_message(chat_id="@cosmos_photos", text="Это все, что я пока могу....:(")
+
+photo = open("./image/nasa_apod_0.jpg", "rb")
+
+bot.send_photo(chat_id="@cosmos_photos", photo=photo)
